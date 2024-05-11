e3a2f7db78f196bef94b2a772540dff3f445cdcc
Dunn, Simeon <simedunn01@gmail.com>
2024-05-11 15:19:15 -0400

    add gsap library

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,20 @@
-export default function Navbar() {
+const navItems = [
+  {label: "Home", url: "/"},
+  {label: "About Us", url: "/about"},
+  {label: "Services", url: "/services"}, 
+  {label: "Reviews", url: "/reviews"},
+  {label: "FAQ", url: "/faq"},
+  {label: "Contact", url: "/contact"}
+];
 
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a>Home</a> 
+        </li>
+      </ul>
+    </nav>
+  );
 }
