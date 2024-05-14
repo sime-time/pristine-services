4fefc8dab17dac4220922a1d7bdbda712267d3db
Dunn, Simeon <simedunn01@gmail.com>
2024-05-14 13:53:22 -0400

    favicon and logo svg

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,7 +8,7 @@ export default function Navigation() {
     <Navbar collapseOnSelect expand="lg" className="bg-body-secondary">
       <Container>
         <Navbar.Brand href="/">
-          <img src="logo.svg" height="80" className="d-inline-block align-top" alt="Company logo" />
+          <img src="logo.svg" height="120" className="d-inline-block align-top" alt="Company logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
