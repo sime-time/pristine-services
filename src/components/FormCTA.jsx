0fcb18050f1343d4b62a39a620df48d015b5442e
Dunn, Simeon <simedunn01@gmail.com>
2024-05-22 17:14:01 -0400

    select dropdown

--- src/components/FormCTA.jsx
+++ src/components/FormCTA.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
-import Dropdown from './Dropdown/Dropdown.jsx';
-import DropdownItem from './Dropdown/DropdownItem.jsx';
+import SelectDropdown from './SelectDropdown/SelectDropdown.jsx'
 
 function FormCTA() {
-  const items = [1, 2, 3, 4, 5, 6, 7]
+
+  const bathrooms = ["1 bathroom", "2 bathroom", "3 bathroom", "4 bathroom", "5 bathroom"];
+  const bedrooms = ["1 bedroom", "2 bedroom", "3 bedroom", "4 bedroom", "5 bedroom"];
+  const cleantype = ["Standard", "Deep Clean",]
 
   return (
-    <Dropdown buttonText="Dropdown button" content={
-      <>
-        {items.map(item => (<DropdownItem key={item}>
-          {`Item ${item}`}
-        </DropdownItem>))}
-      </>
-    } />
+    <div className="container">
+      <h1>Save Time.<br />Reset Your Space.</h1>
+      <div className="d-flex justify-content-start">
+        <SelectDropdown items={bedrooms} />
+        <SelectDropdown items={bathrooms} />
+        <SelectDropdown items={cleantype} />
+        <button className="btn btn-primary btn-lg" type="submit">Book Now</button>
+      </div>
+    </div>
   );
 }
 
