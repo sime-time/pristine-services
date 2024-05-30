6de556a46176b743afd9153a653a47f684acf751
Dunn, Simeon <simedunn01@gmail.com>
2024-05-30 10:53:37 -0400

    merge changes

--- src/components/BookingCalendar.jsx
+++ src/components/BookingCalendar.jsx
@@ -1,10 +1,22 @@
+import { Button, Calendar, CalendarCell, CalendarGrid, Heading } from 'react-aria-components';
+
+
 function BookingCalendar() {
   return (
-    <>
-      <div className="d-flex flex-column justify-content-center align-items-center">
-        <h1 className="display-5">What day should we clean?</h1>
-      </div>
-    </>
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className="display-5">What day should we clean?</h1>
+      <Calendar aria-label="Appointment date">
+        <header>
+          <Button slot="previous">◀</Button>
+          <Heading />
+          <Button slot="next">▶</Button>
+        </header>
+        <CalendarGrid>
+          {(date) => <CalendarCell date={date} />}
+        </CalendarGrid>
+      </Calendar>
+    </div>
+
   );
 }
 
