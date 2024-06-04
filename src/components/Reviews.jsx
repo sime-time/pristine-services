76af5f0786b1a5f323aecbbcab3f739aa101edc1
Dunn, Simeon <simedunn01@gmail.com>
2024-06-04 19:50:58 -0400

    reviews component styles complete

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,26 +1,34 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import { FaStar } from 'react-icons/fa6';
 
 function Reviews() {
   return (
-    <div className="container">
-      <h1>See our Reviews</h1>
-      <Carousel className="d-flex align-content-center">
-        <Carousel.Item>
-          <div className="rounded-3 bg-primary-subtle specific-h-600 w-50">
-            I called on a tuesday afternoon strapped to find a last minute cleaner for after rehearsal, for my sisters beautfil wedding. THANK you to Pristine Clean team. Katie and her were on time and our studio space turn out spotless!
-          </div>
-
-          <Carousel.Caption>
-            <div className="rounded-3 bg-body-secondary shadow text-black">
-              <div>Bobby Hensley</div>
-              <div></div>
+    <div className="container mb-5">
+      <div className="d-flex flex-column align-items-center gap-4">
+        <h1>See what our customers say about us</h1>
+        <Carousel className="w-md-75">
+          <Carousel.Item className="rounded-3 bg-primary-subtle h-100 p-5 fs-3 text-center">
+            <p className="mb-4">
+              I called on a tuesday afternoon strapped to find a last minute cleaner for after rehearsal, for my sisters beautfil wedding. THANK you to Pristine Clean team. Katie and her were on time and our studio space turn out spotless!
+            </p>
+            <div className="d-flex align-items-center justify-content-center fs-4">
+              <div className="rounded-3 bg-body-secondary shadow text-black w-md-50 py-3 px-4">
+                <div className="pb-3 ">Bobby Hensley</div>
+                <div className="p-3 border-top text-primary d-flex gap-2 align-items-center justify-content-center">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </div>
+              </div>
             </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
 
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
   );
 
 
