f92ea58cc49244c46cad052d52de85644bf3b87c
Dunn, Simeon <simedunn01@gmail.com>
2024-06-13 12:26:01 -0400

    change text color and primary color setup in global.css

--- src/components/Contact/ContactPoints.jsx
+++ src/components/Contact/ContactPoints.jsx
@@ -3,7 +3,7 @@ import { AiFillInstagram } from "react-icons/ai";
 
 function ContactPoints() {
   return (
-    <div className="contact-points d-flex flex-column gap-3 w-100 fs-5 mb-5 align-items-center">
+    <div className="contact-points d-flex flex-column gap-3 w-100 fs-5 mb-5 align-items-center rounded-4 bg-primary-subtle p-2">
       <h3 className="display-6 text-center">Reach us directly</h3>
       <a href="tel:3175378111" className="d-flex gap-2 align-items-center" ><span className="fs-4"><FaPhoneAlt /></span>(317) 537-8111</a>
       <a href="mailto:sales@pristineclean-indy.com" className="d-flex gap-2 align-items-center"><span className="fs-4"><FaEnvelope /></span>sales@pristineclean-indy.com</a>
