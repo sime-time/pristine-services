51ab986e08e52bbbbf106529fafd996a15f0ac2b
Dunn, Simeon <simedunn01@gmail.com>
2024-06-20 21:51:16 -0400

    bottom border on hiring process

--- src/components/About/HiringProcess.jsx
+++ src/components/About/HiringProcess.jsx
@@ -12,20 +12,20 @@ function HiringProcess() {
 
   return (
     <>
-      <div className="text-center mt-4">
+      <div className="text-center pt-4">
         <h1 className="display-4 text-center">Our Hiring Process</h1>
         <p className="fs-4">Learn how we provide you with reliable, top-notch cleaners</p>
       </div>
-      <div className="timeline container">
+      <div className="timeline container border-bottom">
         <HiringStep icon={<IoDocumentTextOutline />} title="Online Job Application" isLeft={true} content="The first step is to complete our online job form. If approved, the applicant will move on to phase 2." />
 
         <HiringStep icon={<FaPhoneVolume />} title="Phone Screening" isLeft={false} content="Next, our recruiter will conduct an in-depth phone interview with the candidate" />
 
         <HiringStep icon={<HiUsers />} title="In-Person Meeting" isLeft={true} content="Successful candidates will be invited for an in-person meeting to discuss the role and assess their fit for the team." />
 
-        <HiringStep icon={<FaSearch />} title="Background Check" isLeft={false} content="We will conduct a thorough background check to ensure all candidates meet our standards and requirements." />
+        <HiringStep icon={<FaSearch />} title="Background Check" isLeft={false} content="We will conduct a thorough background check on all candidates." />
 
-        <HiringStep icon={<FaBroom />} title="Training" isLeft={true} content="Finally, new hires will undergo comprehensive training to prepare them for their responsibilities and ensure they are ready to deliver high-quality service." />
+        <HiringStep icon={<FaBroom />} title="Training" isLeft={true} content="Finally, new hires will undergo comprehensive training to prepare them to deliver you high-quality service." />
       </div>
     </>
   );
--- src/components/About/HiringStep.jsx
+++ src/components/About/HiringStep.jsx
@@ -6,7 +6,7 @@ function HiringStep({ icon, title = "Astro", content = "The internet is a big bi
       <div className="tl-icon">{icon}</div>
       <div className="tl-textbox shadow">
         <h2>{title}</h2>
-        <p>{content}</p>
+        <p className="fs-4">{content}</p>
         <span className={isLeft ? 'left-container-arrow' : 'right-container-arrow'}></span>
       </div>
     </div>
